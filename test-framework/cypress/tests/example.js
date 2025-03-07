9bf3af7965e84841ee0407dc1cbd0c8bc013ec87
Nesto149 <49162668+Nesto149@users.noreply.github.com>
2025-03-07 12:59:33 +0100

    test

--- test-framework/cypress/tests/example.js
+++ test-framework/cypress/tests/example.js
@@ -1,6 +1,4 @@
-let myNubmer = 33
-
-function getMyNumber() {
-  console.log(myNumber)
-  let returnMyNumber = myNumber + 24;
-}
+// Stwórz zmienną text z dowolnym zdaniem. Wypisz w konsoli to zdanie i jego długość.
+let text = "To jest zdanie testowe"
+console.log(text)
+console.log(text.length())
