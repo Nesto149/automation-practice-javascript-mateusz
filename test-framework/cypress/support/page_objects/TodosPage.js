d4c98b4acac31594cb5f1efcdd3cb99dbbfcd3f7
Nesto149 <49162668+Nesto149@users.noreply.github.com>
2025-04-14 16:18:04 +0000

    part1

--- test-framework/cypress/support/page_objects/TodosPage.js
+++ test-framework/cypress/support/page_objects/TodosPage.js
@@ -1,5 +1,26 @@
 import { BasePage } from "./BasePage";
 
 export class TodosPage extends BasePage {
-    
+    addTodo(task) {
+        this.typeText('.new-todo', `${task}{enter}`);
+    }
+
+    getTodoListItems() {
+        return this.getElement('.todo-list li');
+    }
+
+    clickActiveFilter() {
+        this.getElement('[data-cy="filter-active"]').click();
+    }
+
+    clickClearCompleted() {
+        this.getElement('.clear-completed').click();
+    }
+
+    editFirstTodo(newText) {
+        this.getElement('.todo-list li').first().dbclick();
+        this.getElement('.todo-list li.editing .edit')
+            .clear()
+            .type(`${newText}{enter}`)
+    }
 }
---
+++ test-framework/cypress/tests/todos.cy.js
@@ -0,0 +1,35 @@
+// cypress/e2e/todos.cy.js
+
+import { TodosPage } from "../support/page_objects/TodosPage";
+
+const todosPage = new TodosPage();
+
+describe('Testy aplikacji TODOS', () => {
+    beforeEach(() => {
+        todosPage.visit();
+    })
+
+    it('Dodanie zadania i sprawdzenie poprawności licznika', () => {
+        todosPage.addTodo('Testowe zadanie');
+        todosPage.getElement('.todo-count').should('contain.text', '1 item left');
+    });
+
+    it('Zabezpieczenie przed SQL Injection', () => {
+        cy.fixture('testData').then((fixtureData) => {
+            todosPage.addTodo(fixtureData.sqlInjection);
+            todosPage.getTodoListItems().should('not.contain.text', 'SELECT * FROM'); //sprawdzamy SQL Injection
+        })
+    })
+
+    it('Unikalność zadań na liście', () => {
+        cy.fixture('testData').then((fixtureData) => {
+            todosPage.addTodo(fixtureData.uniqueTasks[0]);
+            todosPage.addTodo(fixtureData.uniqueTasks[1]);
+            todosPage.addTodo(fixtureData.uniqueTasks[0]);  //dodajemy duplikat
+            // sprawdzamy czy zadanie nie występuje dwa razy
+            todosPage.getTodoListItems().filter(`:contains("${DataTransfer.uniqueTasks[0]}")`).should('have.length', 1);
+        })
+    })
+
+    
+})
